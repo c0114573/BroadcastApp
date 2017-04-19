2e3777449f7a5efacba2c43ca302276b9fa8f66a
C011457331 <c011457331@edu.teu.ac.jp>
2017-04-19 15:45:25 +0900

    Initial commit

---
+++ app/src/main/java/com/c0114573/broadcastapp/ExampleService.java
@@ -0,0 +1,165 @@
+package com.c0114573.broadcastapp;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.math.BigDecimal;
+
+public class ExampleService extends Service implements LocationListener {
+
+    static final String TAG = "ExampleService";
+    private LocationManager mLocationManager;
+
+    BigDecimal x;
+    BigDecimal y;
+
+    String xx;
+    String yy;
+
+
+    int a;
+
+    String b;
+
+    static double latitude1;
+    static double longitude1;
+
+    double latitude2;
+    double longitude2;
+
+    private final int REQUEST_PERMISSION = 1000;
+
+    private LocationManager locationManager;
+
+    static double ido;
+    static double idoido;
+
+    static double keido;
+    static double keidokeido;
+
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, "バックグラウンドサービスを開始しました。", Toast.LENGTH_SHORT).show();
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand Received start id " + startId + ": " + intent);
+
+        if (mLocationManager != null) {
+            Log.d("LocationActivity", "locationManager.requestLocationUpdates");
+            // バックグラウンドから戻ってしまうと例外が発生する場合がある
+            try {
+                // minTime = 1000msec, minDistance = 50m
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    return;
+                }
+
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+
+
+
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                Toast toast = Toast.makeText(this, "例外が発生、位置情報のPermissionを許可していますか？", Toast.LENGTH_SHORT);
+                toast.show();
+//
+//                //MainActivityに戻す
+//                finish();
+            }
+        }
+        //明示的にサービスの起動、停止が決められる場合の返り値
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy");
+//        mLocationManager.removeUpdates(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+//        Toast.makeText(this, "位置情報を更新", Toast.LENGTH_LONG).show();
+
+
+        // 緯度の表示
+//        TextView tv_lat = (TextView) findViewById(R.id.Latitude);
+//        TextView tv_lat = new TextView(this);
+//        tv_lat.setText("緯度:" + location.getLatitude());
+
+        Toast.makeText(this, "緯度"+ location.getLatitude()+"軽度"+location.getLongitude(), Toast.LENGTH_LONG).show();
+
+
+        ido = location.getLatitude();
+        Log.e("GPS", String.valueOf(ido));
+
+
+        x = new BigDecimal(ido);
+        x = x.setScale(7, BigDecimal.ROUND_HALF_UP);
+        Log.e("keta", String.valueOf(x));
+
+        xx = String.valueOf(x);
+        Log.e("xx", xx);
+
+        idoido = Double.parseDouble(xx);
+        Log.e("idoido", String.valueOf(idoido));
+
+        latitude1 = idoido;
+
+        // 経度の表示
+//        TextView tv_lng = (TextView) findViewById(R.id.Longitude);
+//        tv_lng.setText("経度:" + location.getLongitude());
+
+//        int m = r.nextInt(str.length);
+//        Toast.makeText(this, str[m], Toast.LENGTH_LONG).show();
+
+        keido = location.getLongitude();
+        Log.e("GPS", String.valueOf(keido));
+
+        y = new BigDecimal(keido);
+        y = y.setScale(7, BigDecimal.ROUND_HALF_UP);
+        Log.e("keta", String.valueOf(y));
+
+        yy = String.valueOf(y);
+        Log.e("yy", yy);
+
+        keidokeido = Double.parseDouble(yy);
+        longitude1 = keidokeido;
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+}
---
+++ app/src/main/java/com/c0114573/broadcastapp/MainActivity.java
@@ -0,0 +1,144 @@
+package com.c0114573.broadcastapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by C011457331 on 2017/04/19.
+ */
+
+public class MainActivity extends Activity {
+
+    Button bt1,bt2;
+    IntentFilter intentFilter;
+    BootReceiver receiver;
+
+    private final int REQUEST_PERMISSION = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Android 6, API 23以上でパーミッシンの確認
+        if(Build.VERSION.SDK_INT >= 23){
+            checkPermission();
+        }
+        else{
+//            locationActivity();
+            startService(new Intent(getBaseContext(), ExampleService.class));
+
+            receiver = new BootReceiver();
+            intentFilter = new IntentFilter();
+            intentFilter.addAction("BOOT_COMPLETED");
+            registerReceiver(receiver, intentFilter);
+        }
+    }
+
+    // 位置情報許可の確認
+    public void checkPermission() {
+        // 既に許可している
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+//            locationActivity();
+            startService(new Intent(getBaseContext(), ExampleService.class));
+
+            receiver = new BootReceiver();
+            intentFilter = new IntentFilter();
+            intentFilter.addAction("BOOT_COMPLETED");
+            registerReceiver(receiver, intentFilter);
+        }
+        // 拒否していた場合
+        else{
+            requestLocationPermission();
+        }
+    }
+
+    // 許可を求める
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION);
+
+        } else {
+            Toast toast = Toast.makeText(this, "許可されないとアプリが実行できません", Toast.LENGTH_SHORT);
+            toast.show();
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, REQUEST_PERMISSION);
+
+        }
+    }
+
+    // 結果の受け取り
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                locationActivity();
+                startService(new Intent(getBaseContext(), ExampleService.class));
+
+                receiver = new BootReceiver();
+                intentFilter = new IntentFilter();
+                intentFilter.addAction("BOOT_COMPLETED");
+                registerReceiver(receiver, intentFilter);
+                return;
+
+            } else {
+                // それでも拒否された時の対応
+                Toast toast = Toast.makeText(this, "これ以上なにもできません", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+
+
+    LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        setContentView(ll);
+
+        bt1 = new Button(this);
+        bt2 = new Button(this);
+
+        bt1.setText("開始");
+        bt2.setText("停止");
+
+        TextView tv_lat = new TextView(this);
+        tv_lat.setText("軽度");
+
+        ll.addView(bt1);
+        ll.addView(bt2);
+        ll.addView(tv_lat);
+
+        bt1.setOnClickListener(new SampleClickListener());
+        bt2.setOnClickListener(new SampleClickListener());
+    }
+
+    class SampleClickListener implements View.OnClickListener {
+        public  void onClick(View v){
+            if(v == bt1){
+                startService(new Intent(getBaseContext(), ExampleService.class));
+
+                receiver = new BootReceiver();
+                intentFilter = new IntentFilter();
+                intentFilter.addAction("BOOT_COMPLETED");
+                registerReceiver(receiver, intentFilter);
+            }
+            else if(v == bt2){
+
+            }
+        }
+    }
+}
